bf1808947f45b6d218d98e783b3ca724379140ab
lobak <lobak070811@gmail.com>
2026-08-21 16:54:03 +0800

    declare in header

--- header.h
+++ header.h
@@ -2,14 +2,18 @@
 #define MY_HEADER
 
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <iomanip>
-#include <sstream>
+
+void mainMenu;
+void registerMenu;
+void studentRegister;
+void ownerRegister;
+void agentRegister;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char** argv) {
+int main(){
+	
+	mainMenu();
+	
 	return 0;
 }
 
