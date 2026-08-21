aa7fe376bf73c8d6c8bfeb471ed63027d8456260
lobak <lobak070811@gmail.com>
2026-08-21 19:11:38 +0800

    delete main funciton, declare login file

--- header.h
+++ header.h
@@ -4,17 +4,14 @@
 #include <iostream>
 
 void mainMenu;
+
 void registerMenu;
 void studentRegister;
 void ownerRegister;
 void agentRegister;
 
+void login_menu;
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int main(){
-	
-	mainMenu();
-	
-	return 0;
-}
 
 #endif //end code
